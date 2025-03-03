79318b3b9a1e2f5a9ad1829c19471fdccfaa4647
Hugh Saalmans <hugh.saalmans@gmail.com>
2025-03-03 17:51:32 +1100

    added basic download script

---
+++ xx_download_and_copy_to_s3.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+AWS_PROFILE="minus34"
+FTP_URL="ftp://anonymous:anonymous@ftp.bom.gov.au:21/anon/gen/fwo"
+WORKING_FOLDER="/Users/$(whoami)/tmp/bom"
+
+
+curl --insecure "${FTP_URL}/IDQ65248.zip" -o "${WORKING_FOLDER}/IDQ65248.zip"
+curl --insecure "${FTP_URL}/IDQ65252.zip" -o "${WORKING_FOLDER}/IDQ65252.zip"
+
+aws --profile=${AWS_PROFILE} s3 sync ${WORKING_FOLDER} s3://minus34.com/opendata/bom/cyclone/2025/alfred --acl public-read
+
+
+
+
+
+
+
+
