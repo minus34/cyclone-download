f499a1a4810ede4487c78343c27144991b557745
Hugh Saalmans <hugh.saalmans@gmail.com>
2025-03-03 17:58:50 +1100

    added basic download script

--- xx_download_and_copy_to_s3.sh
+++ xx_download_and_copy_to_s3.sh
@@ -4,7 +4,6 @@ AWS_PROFILE="minus34"
 FTP_URL="ftp://anonymous:anonymous@ftp.bom.gov.au:21/anon/gen/fwo"
 WORKING_FOLDER="/Users/$(whoami)/tmp/bom"
 
-
 curl --insecure "${FTP_URL}/IDQ65248.zip" -o "${WORKING_FOLDER}/IDQ65248.zip"
 curl --insecure "${FTP_URL}/IDQ65252.zip" -o "${WORKING_FOLDER}/IDQ65252.zip"
 
@@ -12,6 +11,13 @@ aws --profile=${AWS_PROFILE} s3 sync ${WORKING_FOLDER} s3://minus34.com/opendata
 
 
 
+# crontab commands
+
+#crontab -e  # i to start editing, ESC to stop, :wq to save and exit
+#
+#crontab -l  # list all cron jobs
+#
+#@hourly /Users/hughsaalmans/git/minus34/cyclone-download/xx_download_and_copy_to_s3.sh
 
 
 
